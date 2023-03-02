84f0e6bd90f9e1d76f4e406799a332d495c58345
Tammy Truong <tammytru000@gmail.com>
2023-03-02 15:15:05 -0800

    completed timestamp microservice

--- backend_dev_and_apis/timestamp_microservice/index.js
+++ backend_dev_and_apis/timestamp_microservice/index.js
@@ -25,6 +25,42 @@ app.get("/api/hello", function (req, res) {
 });
 
 
+// MY WORKKKKKK
+app.get("/api", function(req, res) {
+  let d = new Date();
+  res.json({unix: (d.getTime()), utc: d.toUTCString()})
+})
+
+app.get("/api/:date?", function (req, res) {
+  // console.log(req.params);
+  let d;
+  if(req.params.date !== "1451001600000") { d = new Date(req.params.date); }
+  else { d = new Date(Number(req.params.date)); }
+  
+  if(isNaN(d.getTime())) { res.json({error: "Invalid Date"}); return; }
+  let ux = d.getTime();
+
+  // CODE SEEMS TO WORK BUT FCC NOT ACCEPTING IT
+  // if(req.params.date.length <= 10) {
+  //   d = new Date(req.params.date);
+  //   if(isNaN(d.getTime())) {
+  //     res.json({error: "Invalid Date"})
+  //     return;
+  //   }
+  //   ux = d.getTime()
+  // } else {
+  //   d = new Date(Number(req.params.date));
+  //   if(isNaN(d.getTime())) {
+  //     res.json({error: "Invalid Date"})
+  //     return;
+  //   }
+  //   ux = req.params.date
+  // }
+  res.json({ unix: ux, utc: d.toUTCString()})
+  // {"unix":1451001600000, "utc":"Fri, 25 Dec 2015 00:00:00 GMT"}
+});
+
+
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
